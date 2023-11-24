cc335a1803ffb24e929ce217e96cec75c3970e15
hydrusbeta <hydrusgamma@gmail.com>
2023-11-24 11:38:13 -0700

    Added a callback for retrieving GPU information and updated hay_say_common version in Dockerfile.

--- main.py
+++ main.py
@@ -56,6 +56,10 @@ def register_methods(cache):
 
         return json.dumps(response, sort_keys=True, indent=4), code
 
+    @app.route('/gpu-info', methods=['GET'])
+    def get_gpu_info():
+        return get_gpu_info_from_another_venv(PYTHON_EXECUTABLE)
+
     def parse_inputs():
         schema = {
             'type': 'object',
